2a263db1c96ce816946cf1c2beb911597b8557b3
PrinceJonaa <jonathanbonner128@gmail.com>
2022-01-24 11:11:54 -0500

    Pushed code I had backed up

---
+++ Project 2/rockpaperscissors.js
@@ -0,0 +1,6 @@
+const lapis= 0;
+const papyrus = 1;
+const scalpellus = 2;
+
+let playerChoice = prompt("Do you choose lapis, papyrus or scalpellus?");
+let computerChoice = Math.floor(Math.random() * 3);
---
+++ Project 3/js.js
@@ -0,0 +1,5 @@
+const choices = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+let playerChoice = "";
+let computerChoice = "";
